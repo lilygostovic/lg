864f08698bc1772a81bc6975f67f98f3abaf17ab
Lily Gostovic <lily.gostovic@gmail.com>
2022-01-02 11:23:37 -0500

    Updated portfolio page design.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 // importing components
 import Nav from '../sections/Nav';
@@ -7,6 +8,7 @@ import Nav from '../sections/Nav';
 // importing pictures
 import background from '../images/home2.jpg';
 import blog from '../images/blog.jpg';
+import blogImg from '../images/blog.jpg';
 import bpe from '../images/bpe.jpg';
 
 
@@ -50,55 +52,92 @@ const Spacing = styled.div`
     padding-top: 200px;
     box-shadow: inset 0 0 0 1000px rgba(255,255,255,0.65);
 `;
-const IntegratedExternalLink = styled.a`
-    color: grey;
-    text-decoration: none;
-
-    transition: all 0.3 ease;
-    &:hover {
-        color: white;
-    }
-`;
-const ProjectDiv = styled.div`
-    padding: 0 20% 350px 20%;
-    text-align: center;
-    display: inline-flex;
-`;
-const LeftDiv = styled.div`
-    padding-right: 5%;
-    text-align: left;
-    vertical-align: middle;
-`;
-const RightDiv = styled.div`
-    padding-left: 5%;
-    text-align: right;
-    vertical-align: middle;
-`;
-const ProjectVisualPic = styled.img`
+const ProjectBackground = styled.img`
+    width: 100%;
+    height: 100%;
     object-fit: cover;
-    opacity: 0.9;
-    height: 400px;
-    width: 350px;
-    box-shadow: inset 0 0 0 1000px rgba(255,255,255,0.65);
+    transition: all 0.5s ease;
 `;
-const ProjectTitle = styled.h2`
-    text-transform: uppercase;
+const ProjectDiv1 = styled.div`
+    padding-bottom: 300px;
+`;
+const ProjectHeader = styled.h3`
+    position: absolute;
+    display: none;
+    
+    top: 5%;
+    left: 10%;
+
+    /* text-transform: uppercase; */
     font-family: 'Montserrat', sans-serif;
-    font-weight: 300;
-    margin: 0;
-    padding-top: 30px;
+    font-weight: 400;
+    color: white;
+    font-size: 20px;
 `;
 const ProjectDescription = styled.p`
-    padding-top: 25px;
-    line-height: 25px;
+    position: absolute;
+    display: none;
     margin: 0;
+
+    left: 10%;
+    right: 10%;
+    top: 20%;
+    
+    line-height: 25px;
+    color: white;
 `;
 const ProjectSkills = styled.p`
-    font-weight: 400;
+    position: absolute;
+    display: none;
     margin: 0;
-    padding-top: 15px;
-`;
 
+    left: 10%;
+    right: 10%;
+    top: 70%;
+
+    font-weight: 400;
+    color: white;
+`;
+const ProjectBackgroundContainer = styled.div`
+    margin: auto; // centers div
+    
+    background-color: pink;
+    position: relative;
+
+    height: 30vw;
+    width: 50vw;
+
+    transition: all 0.3s ease;
+    &:hover{
+        ${ProjectBackground} {
+            opacity: 25%;
+        }
+        ${ProjectHeader} {
+            display: inline;
+        }
+        ${ProjectDescription}{
+            display: inline;
+        }
+        ${ProjectSkills}{
+            display: inline;
+        }
+        background-color: rgb(0,0,0);
+        transform: scale(1.05);
+    }
+`;
+const ExternalLink = styled.a`
+  text-decoration: none;
+`;
+const IntegratedLink = styled.a`
+    text-decoration: none;
+    color: white;
+    
+    transition: all 0.3s ease;
+    &:hover{
+        color: grey;
+    }
+`;
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 function scrollToTop() {
     window.scrollTo(0, 0);
@@ -120,37 +159,54 @@ const Experience = () => {
             <Spacing/>
 
             <MainBody>
+                <ProjectDiv1>
+                    <ExternalLink href={'https://github.com/lilygostovic/blog'} target="_blank">
+                        <ProjectBackgroundContainer>
+                            <ProjectBackground src={blogImg} />
+                            <ProjectHeader>
+                                <IntegratedLink href={'https://lily-gostovic-blog.herokuapp.com'} target="_blank">Lily's Diaries</IntegratedLink>
+                            </ProjectHeader>
+                            <ProjectDescription>
+                                A personal blog using create-react-app. A passion project created to
+                                practice and enhance skills in HTML, CSS, Javascript, and React. This
+                                blog houses essays written on a variety of topics, some amateur
+                                photography, as well as blog posts on my travels, fitness, and mental
+                                health. Developing this passion project during initial stages of the
+                                COVID-19 pandemic in the spring of 2020 taught me the basics of web
+                                development, including HTML, CSS and Javascript. The skills I quickly
+                                learned in developing the website for my blog allowed me to easily
+                                create this website you are reading right now!
+                            </ProjectDescription>
+                            <ProjectSkills>
+                                Javascript, React, Nodejs, HTML, CSS
+                            </ProjectSkills>
+                        </ProjectBackgroundContainer>
+                    </ExternalLink>
+                </ProjectDiv1>
+                <ProjectDiv1>
+                    <ExternalLink href={'https://github.com/lilygostovic/bpe'} target="_blank">
+                        <ProjectBackgroundContainer>
+                            <ProjectBackground src={bpe} />
+                            <ProjectHeader>Web Scraper</ProjectHeader>
+                            <ProjectDescription>
+                                During the Winter 2021 semester and following summer,
+                                I worked as a research assistant for professor Daniel Rubenson's research
+                                group. Dr. Rubenson is a professor of political science at
+                                Ryerson University who researches questions of political participation
+                                and political economy in a variety of settings. I developed software
+                                for his research group that scrapes data from the internet and neatly
+                                parses it into a .csv file. To complete this project, I used Python
+                                and the Python HTML parser package BeautifulSoup.
+                            </ProjectDescription>
+                            <ProjectSkills>
+                                Python
+                            </ProjectSkills>
+                        </ProjectBackgroundContainer>
+                    </ExternalLink>
+                </ProjectDiv1>
+            </MainBody>
 
-                <ProjectDiv>
-
-                    <LeftDiv>
-
-                        <ProjectTitle>Personal Blog</ProjectTitle>
-
-                        <ProjectDescription>
-                            A personal <IntegratedExternalLink href='https://lily-gostovic-blog.herokuapp.com' target="_blank">blog</IntegratedExternalLink> created
-                            using create-react-app. A passion project created to practice and
-                            enhance skills in HTML, CSS, Javascript, and React. This blog houses
-                            essays written on a variety of topics, some amateur photography,
-                            as well as blog posts on my travels, fitness, and mental health. 
-                            Developing this passion project during initial stages of the
-                            COVID-19 pandemic in the spring of 2020 taught me the basics of web
-                            development, including HTML, CSS and Javascript. The skills I quickly
-                            learned in developing the website for my blog allowed me to easily
-                            create this website you are reading right now!
-                        </ProjectDescription>
-
-                        <ProjectSkills>
-                            Javascript, React, Nodejs, HTML, CSS
-                        </ProjectSkills>
-
-                    </LeftDiv>
-
-                    <RightDiv>
-                        <ProjectVisualPic src={blog}/>
-                    </RightDiv>
-
-                </ProjectDiv>
+            {/* <MainBody>
 
                 <ProjectDiv>
 
@@ -180,9 +236,9 @@ const Experience = () => {
                     </RightDiv>
 
                 </ProjectDiv>
-            </MainBody>
+            </MainBody> */}
 
-            <Spacing/>
+            {/* <Spacing/> */}
         </Main>
     );
 }
